3f7b956c7fc37c99519b51e4c7f16fe48f905a40
Vincent Miller <vmiller0725@gmail.com>
2019-01-27 15:01:27 -0500

    asdf

--- Assets/Scripts/ChonkMeter.cs
+++ Assets/Scripts/ChonkMeter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ChonkMeter : MonoBehaviour {
@@ -10,8 +11,12 @@ public class ChonkMeter : MonoBehaviour {
     public GameObject world;
     public Eater heEats;
 
+    public MusicFade gameMusic;
+    public float endDuration;
+
     private RectTransform[] levels;
     private CanvasGroup[] levelImages;
+    private bool ended;
 
     // Start is called before the first frame update
     void Start() {
@@ -40,8 +45,15 @@ public class ChonkMeter : MonoBehaviour {
 
         levelImages[heEats.curLevel].alpha = 1;
 
-        if (!world) {
+        if (!world && !ended) {
             anim.SetTrigger("GameOver");
+            gameMusic.targetFadeLevel = 0;
+            ended = true;
+            Invoke("End", endDuration);
         }
+    }
+
+    private void End() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
